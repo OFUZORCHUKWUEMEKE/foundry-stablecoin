da161b0ea21ed57cd2e1eee8f0a68eac9e461412
Ofuzor chukwuemeke <uniben2018@gmail.com>
2024-10-26 19:43:30 +0100

    deploy script

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -4,8 +4,27 @@ pragma solidity ^0.8.13;
 import {Script, console} from "forge-std/Script.sol";
 import {DecentralizedStableCoin} from "../src/DecentralizesStableCoin.sol";
 import {DSCEngine} from "../src/DSCEngine.sol";
+import { HelperConfig } from "./HelperConfig.s.sol";
 
 contract DeployDsc is Script{
+    address[] public tokenAddreses;
+    address[] public priceFeedAddresses;
+
+    function run() external returns(DecentralizedStableCoin,DSCEngine){
+        HelperConfig config = new HelperConfig();
+
+        (address wethUsdPriceFeed , address wethUsdPriceFeed, address weth , address wbtc, uint256 deployerKey) = config.activeNetworkConfig();
+
+        tokenAddreses = [weth,wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed,wbtcUsdPriceFeed];
+
+        vm.startBroadcast();
+        DecentralizedStableCoin dsc = new DecentralizedStableCoin();
+        DSCEngine engine = new DSCEngine(tokenAddreses,priceFeedAddresses,address(dsc));
+        dsc.transferOwnership(engine);
+        vm.stopBroadcast();
+        return (dsc,engine);
+    }
 
 }
 
